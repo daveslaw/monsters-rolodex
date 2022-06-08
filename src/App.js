64c0c3f897ae5f4a21805884d16621c598c37bcb
Dave <daveslaw@gmail.com>
2022-06-08 22:11:47 +0300

    api and filtering working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+		};
+		console.log("constructor");
+	}
+
+	componentDidMount() {
+		console.log("componentDidMount");
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) =>
+				this.setState(
+					() => {
+						return { monsters: users };
+					},
+					() => {
+						console.log(this.state);
+					}
+				)
+			);
+	}
+
+	render() {
+		console.log("Render");
+		return (
+			<div className="App">
+				<input
+					className="search-box"
+					type="search"
+					placeholder="Search monsters"
+					onChange={(event) => {
+						const searchString = event.target.value.toLocaleLowerCase();
+						const filteredMonsters = this.state.monsters.filter((monster)=> {
+							return monster.name.toLocaleLowerCase().includes(searchString)
+						})
+						this.setState(()=>{
+							return { monsters: filteredMonsters}
+						})
+						console.log(event.target.value);
+					}}
+				/>
+				{this.state.monsters.map((monster) => {
+					return <h1 key={monster.id}>{monster.name}</h1>;
+				})}
+			</div>
+		);
+	}
 }
+
+App.propTypes = {};
 
 export default App;
